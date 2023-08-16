652dcd5f80dba6fcfeff88591acb8be345b1c2e4
Filip Stojanovic <philsty92@gmail.com>
2023-08-16 13:58:51 -0400

    testing

--- app/team/[slug]/page.js
+++ app/team/[slug]/page.js
@@ -33,8 +33,7 @@ export default async function Member({ params }) {
 	return (
 		<main>
 			<section>
-				{/* <TeamMemberSection member={member} /> */}
-				test
+				<TeamMemberSection member={member} />
 			</section>
 		</main>
 	)
--- components/TeamMemberSection.js
+++ components/TeamMemberSection.js
@@ -7,41 +7,45 @@ import Link from 'next/link'
 
 const TeamMemberSection = ({ member }) => {
 	return (
-		<div className='sectionContainer'>
-			<div className={styles.photo}>
-				<Image
-					src={'https:' + member.fields.photo.fields.file.url}
-					fill
-					quality={100}
-					sizes='(max-width: 768px) 100vw, 768px'
-					style={{ objectFit: 'cover' }}
-					alt='Section Image'
-					priority={true}
-					as='img'
-				/>
-			</div>
-			<div className={styles.bio}>
-				<div>
-					<h3>{member.fields.name}</h3>
-					<p>{member.fields.title}</p>
-				</div>
+		<>
+			{member && (
+				<div className='sectionContainer'>
+					<div className={styles.photo}>
+						<Image
+							src={'https:' + member.fields.photo.fields.file.url}
+							fill
+							quality={100}
+							sizes='(max-width: 768px) 100vw, 768px'
+							style={{ objectFit: 'cover' }}
+							alt='Section Image'
+							priority={true}
+							as='img'
+						/>
+					</div>
+					<div className={styles.bio}>
+						<div>
+							<h3>{member.fields.name}</h3>
+							<p>{member.fields.title}</p>
+						</div>
 
-				<p>{member.fields.bio}</p>
+						<p>{member.fields.bio}</p>
 
-				<div className={styles.links}>
-					{member.fields.linkedin && (
-						<Link href={member.fields.linkedin}>
-							<p className={styles.link}>Visit LinkedIn</p>
-						</Link>
-					)}
-					{member.fields.vcard && (
-						<Link href={member.fields.vcard.fields.file.url}>
-							<p className={styles.link}>Download vCard</p>
-						</Link>
-					)}
+						<div className={styles.links}>
+							{member.fields.linkedin && (
+								<Link href={member.fields.linkedin}>
+									<p className={styles.link}>Visit LinkedIn</p>
+								</Link>
+							)}
+							{member.fields.vcard && (
+								<Link href={member.fields.vcard.fields.file.url}>
+									<p className={styles.link}>Download vCard</p>
+								</Link>
+							)}
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
+			)}
+		</>
 	)
 }
 
