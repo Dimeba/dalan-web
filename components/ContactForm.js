aef6da2c30600e068e4f95dec365804a9cb369fb
Filip Stojanovic <philsty92@gmail.com>
2023-08-30 08:02:41 -0400

    Updated contact form subject options.

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -12,13 +12,15 @@ const ContactForm = () => {
 			<input type='hidden' name='form-name' value='contact' />
 
 			<input id='name' name='name' type='text' placeholder='Name' />
-			<select id='inquary' name='inquiryType' defaultValue='inquiryType'>
+			<select id='inquary' name='inquiryType' defaultValue='defaultOption'>
 				<option disabled hidden value='defaultOption'>
-					How can we help?
+					Subject
 				</option>
-				<option value='type1'>Type 1</option>
-				<option value='type2'>Type 2</option>
-				<option value='type3'>Type 3</option>
+				<option value='type1'>Acquisitions</option>
+				<option value='type2'>Credit</option>
+				<option value='type3'>Management</option>
+				<option value='type3'>Leasing</option>
+				<option value='type3'>General Inquiry</option>
 			</select>
 
 			<input type='text' id='phone' name='phone' placeholder='Phone Number' />
